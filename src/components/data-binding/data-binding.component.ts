a7d97f7104be7eb927c5b89b15d83975913ca304
Kakade Alpana <alpanakakde@gmail.com>
2025-05-27 12:48:17 +0530

    -m "Data Binding"

--- src/components/data-binding/data-binding.component.ts
+++ src/components/data-binding/data-binding.component.ts
@@ -1,13 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal} from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-data-binding',
-  imports: [],
+  imports: [FormsModule],
+   standalone: true,
   templateUrl: './data-binding.component.html',
   styleUrl: './data-binding.component.css'
 })
 export class DataBindingComponent implements OnInit{
+
+  newData :string = "Angular 19";
+  inputType ='Checkbox';
+  totalCount : number = 101;
+  isPresent :boolean = true;
+
+  studentData :string ="bg-success";
+
+  cityName :string ="Pune";
+
+  // signal declaration
+  personalName = signal("Alpana Pawar")
  
   constructor(private router : Router){
 
@@ -15,7 +29,17 @@ export class DataBindingComponent implements OnInit{
   ngOnInit(): void {
    
 }
+// beow for routing from html 
   gotoAdminPanal(){
     this.router.navigateByUrl('admin')
+  }
+  showMessage(){
+    alert("Hello , welcome in your data");
+    console.log("hey,you click me")
+  }
+  changeVariblename(){
+    this.newData ="React new version";
+    // signal value change with set method
+    this.personalName.set("Piyusha Pawar")
   }
 }
