d5d159964d4349b34f597ac7fbd664f436d8cf5b
Kakade Alpana <alpanakakde@gmail.com>
2025-06-08 10:22:15 +0530

    fomatted file

--- src/components/api-get-post/api-get-post.component.ts
+++ src/components/api-get-post/api-get-post.component.ts
@@ -8,43 +8,40 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './api-get-post.component.html',
   styleUrl: './api-get-post.component.css'
 })
-export class ApiGetPostComponent implements OnInit{
+export class ApiGetPostComponent implements OnInit {
 
-  
- http = inject(HttpClient)
 
-  deptData :any[]=[];
+  http = inject(HttpClient)
 
-departObj :any={
-  // departmentId: 0,
-  departmentName: "",
-  departmentLogo: ""
-}
+  deptData: any[] = [];
+
+  departObj: any = {
+    // departmentId: 0,
+    departmentName: "",
+    departmentLogo: ""
+  }
   ngOnInit(): void {
-    // this.getDepartment();
   }
 
-    onSubmit(){
-      console.log('Sending:', this.departObj);
-      
+  onSubmit() {
+    console.log('Sending:', this.departObj);
+    this.http.post('https://projectapi.gerasim.in/api/EmployeeManagement/AddNewDepartment', this.departObj).subscribe((res: any) => {
 
-      this.http.post('https://projectapi.gerasim.in/api/EmployeeManagement/AddNewDepartment',this.departObj).subscribe((res:any)=>{
-      
-         this.getDepartment() 
-        // if(res.result){
-        //   alert("dept created succesfully !")
+      this.getDepartment()
+      // if(res.result){
+      //   alert("dept created succesfully !")
 
-        // }else{
-        //   alert("Dept not created")
-        // }
+      // }else{
+      //   alert("Dept not created")
+      // }
 
-      })
-    }
+    })
+  }
 
-    getDepartment(){
-      this.http.get('https://projectapi.gerasim.in/api/EmployeeManagement/GetParentDepartment').subscribe((result:any)=>{
-       console.log(result, "get")
-        this.deptData =result.data;
-      })
-    }
+  getDepartment() {
+    this.http.get('https://projectapi.gerasim.in/api/EmployeeManagement/GetParentDepartment').subscribe((result: any) => {
+      console.log(result, "get")
+      this.deptData = result.data;
+    })
+  }
 }
